38ef3d2592d5bde9ea45446e4a447bc39a25cc5d
robsonw1 <robsonwilliam1008@gmail.com>
2025-10-21 16:46:00 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const app = express();
 // CORS - MIDDLEWARE MANUAL (ADICIONADO NO INÍCIO)
 // ========================================
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://app-forneiro-eden-frontend.ilewqk.easypanel.host');
+  res.header('Access-Control-Allow-Origin', 'https://forneiro-eden-app.aezap.site/');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
